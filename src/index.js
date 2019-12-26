fde610936f9dbcf5bb57188a3e44a21ab428c832
Haikel Fazzani <haikel.fazzani@hotmail.com>
2019-12-26 09:40:57 +0100

    update app

--- src/index.js
+++ src/index.js
@@ -2,7 +2,6 @@ var editor = ace.edit("editor");
 ace.require("ace/ext/language_tools");
 editor.setTheme("ace/theme/monokai");
 editor.session.setMode("ace/mode/html");
-document.getElementById('editor').style.fontSize = '18px';
 
 editor.setOptions({
   enableBasicAutocompletion: true,
@@ -12,8 +11,31 @@ editor.setOptions({
   copyWithEmptySelection: true,
 });
 
-var iframe = document.getElementById("code");
-var result = `
+var iframeElement = document.getElementById("code");
+var editorElement = document.getElementById('editor');
+
+var editorConfig = {
+  defaultFontSize: 18
+};
+
+/** Font size */
+editorElement.style.fontSize = editorConfig.defaultFontSize + 'px';
+document.getElementById('btn-font-plus').addEventListener('click', () => {
+  editorConfig.defaultFontSize++;
+  editorElement.style.fontSize = editorConfig.defaultFontSize + 'px';
+})
+
+document.getElementById('btn-font-minus').addEventListener('click', () => {
+  editorConfig.defaultFontSize--;
+  editorElement.style.fontSize = editorConfig.defaultFontSize + 'px';
+})
+
+/** get saved code from localStorage */
+var userCode = '';
+if (localStorage.getItem('code-save')) {
+  userCode = JSON.parse(localStorage.getItem('code-save'))
+} else {
+  userCode = `
 <button id="btn">click</button>
 <p id="res"></p>
 <script>
@@ -21,14 +43,20 @@ document.getElementById('btn').addEventListener('click' , () =>{
     document.getElementById('res').textContent = 'hello world'
 })
 </script>`;
-editor.setValue(result);
-iframe.src = 'data:text/html;charset=utf-8,' + encodeURI(result);
+}
+
+editor.setValue(userCode);
+iframeElement.src = 'data:text/html;charset=utf-8,' + encodeURI(userCode);
 
 editor.session.on('change', function (delta) {
-  result = editor.getValue();
+  userCode = editor.getValue();
 });
 
-
 document.getElementById('btn-run').addEventListener('click', () => {
-  iframe.src = 'data:text/html;charset=utf-8,' + encodeURI(result);
+  iframeElement.src = 'data:text/html;charset=utf-8,' + encodeURI(userCode);
+})
+
+/** save code into localstorage */
+document.getElementById('btn-save').addEventListener('click', () => {
+  localStorage.setItem('code-save', JSON.stringify(userCode))
 })
