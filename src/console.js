1e04893d31f039621fa4f029a9ea8f291eddc7e5
Haikel Fazzani <haikel.fazzani@hotmail.com>
2019-12-28 20:41:19 +0100

    update app

--- src/console.js
+++ src/console.js
@@ -10,36 +10,30 @@ document.getElementById('close-console').addEventListener('click', () => {
 
 document.getElementById('btn-run').addEventListener('click', () => {
 
-  const url = getGeneratedPageURL({
-    html: '<ul id="log"></ul>',
-    css: `
-  ul {background-color: #272822 !important; list-style:none; padding:0; margin:0; }
-  li { 
-    padding: 10px 20px;
-    color: #fff;
-    border-bottom: 4px solid #2f3129;
-  }
-  `,
-    js: `window.onload = () => {
-    (function () {
-      if (!console) {
-        console = {};
-      }
-      var old = console.log;
-      var logger = document.getElementById('log');
-      console.log = function (message) {
-        if (typeof message == 'object') {
-          logger.innerHTML += '<li>' + (JSON && JSON.stringify ? JSON.stringify(message) : String(message)) + '</li>';
-        } else {
-          logger.innerHTML += '<li>' + message + '</li>';
+  if (editorConfig.codeSave.js && editorConfig.codeSave.js.length > 10) {
+
+    var iframeConsole = document.querySelector('.iframe-console>ul');
+
+    axios.get(`https://box-server.unubo.app/runcode?lang=javascript&content=${editorConfig.codeSave.js}`)
+      .then(resp => resp.data)
+      .then(r => {
+        
+        iframeConsole.innerHTML = '';
+        const successRes = r.data.stdout.replace(/\n/ig, '-').split('-');
+
+        successRes.forEach(d => {
+          if (d && d.length > 0 && iframeConsole) {
+            iframeConsole.innerHTML += `<li>${d}</li>`;
+          }
+        });
+
+        if (r.data.stderr && r.data.stderr.length > 10) {
+          iframeConsole.innerHTML = `<li style="color:#dc3545">${r.data.stderr}</li>`;
         }
-      }
-  })();
-  ${editorConfig.codeSave.js}
-  }`
-  });
 
-  const iframeConsole = document.querySelector('#iframe-console');
-  iframeConsole.src = url;
-
+      })
+      .catch(e => {
+        console.log(e)
+      })
+  }
 });
