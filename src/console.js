659fca6b50ccd661e324715c155c7c68e83571c4
Haikel Fazzani <haikel.fazzani@hotmail.com>
2020-01-30 14:37:05 +0100

    fix bug: console

--- src/console.js
+++ src/console.js
@@ -8,32 +8,13 @@ document.getElementById('close-console').addEventListener('click', () => {
   ifCons.style.display = ifCons.style.display === 'block' ? 'none' : 'block';
 })
 
+
 document.getElementById('btn-run').addEventListener('click', () => {
 
   if (editorConfig.codeSave.js && editorConfig.codeSave.js.length > 10) {
 
     var iframeConsole = document.querySelector('.iframe-console>ul');
 
-    axios.get(`https://box-server.unubo.app/runcode?lang=javascript&content=${editorConfig.codeSave.js}`)
-      .then(resp => resp.data)
-      .then(r => {
-        
-        iframeConsole.innerHTML = '';
-        const successRes = r.data.stdout.replace(/\n/ig, '-').split('-');
-
-        successRes.forEach(d => {
-          if (d && d.length > 0 && iframeConsole) {
-            iframeConsole.innerHTML += `<li>${d}</li>`;
-          }
-        });
-
-        if (r.data.stderr && r.data.stderr.length > 10) {
-          iframeConsole.innerHTML = `<li style="color:#dc3545">${r.data.stderr}</li>`;
-        }
-
-      })
-      .catch(e => {
-        console.log(e)
-      })
+    iframeConsole.innerHTML = eval(editorConfig.codeSave.js );
   }
 });
