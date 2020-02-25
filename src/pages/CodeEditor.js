6da22dc26eb9612a392d9c31cbb39fd1d22a9439
Haikel Fazzani <haikel.fazzani@hotmail.com>
2020-02-25 21:19:27 +0100

    save split pane sizes

--- src/pages/CodeEditor.js
+++ src/pages/CodeEditor.js
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { useState } from 'react';
 import Split from 'react-split';
 import Editor from '../containers/Editor';
 import Sidebar from '../containers/Sidebar';
@@ -6,11 +6,21 @@ import CodeResult from '../containers/CodeResult';
 import '../styles/code-editor.css';
 
 export default function CodeEditor () {
+
+  const [sizes, setSizes] = useState(() => {
+    let localSizes = localStorage.getItem('kody-split-pane');
+    return localSizes ? JSON.parse(localSizes) : [50, 50];
+  });
+
+  const onDragEnd = v => {
+    localStorage.setItem('kody-split-pane', JSON.stringify(v));
+  }
+
   return <main>
     <Sidebar />
 
     <div className="container">
-      <Split sizes={[50, 50]} minSize={0} gutterSize={7} gutterAlign="center" direction="horizontal">
+      <Split sizes={sizes} onDragEnd={onDragEnd} minSize={0} gutterSize={7} gutterAlign="center" direction="horizontal">
         <Editor />
         <CodeResult />
       </Split>
