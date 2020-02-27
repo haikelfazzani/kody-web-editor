518fd4285ee142592406387efe97a91a8f64c715
Haikel Fazzani <haikel.fazzani@hotmail.com>
2020-02-27 14:20:03 +0100

    feat: console

--- src/util/console.js
+++ src/util/console.js
@@ -1,20 +1,30 @@
 let ov = `
 var origLog = console.log;
-// here, the contents of console.log will be stored
 var consoleBuffer = [];
 
-// replace console.log with own function
+window.onerror = function (msg, url, lineNo, columnNo, error) {
+  var string = msg.toLowerCase();
+  var substring = "script error";
+  if (string.indexOf(substring) > -1){
+    consoleBuffer.push('Script Error: See Browser Console for Detail');
+  } else {
+    var message = [
+      'Message: ' + msg ,
+      '<br>Line: ' + (lineNo-28) ,
+      '<br>Column: ' + columnNo ,
+      '<br>Error object: ' + JSON.stringify(error)
+    ].join(' ');
+
+    consoleBuffer.push('<span style="color: #f35c5c">'+ message +'</span>');
+  }
+
+  return false;
+};
+
 console.log = function () {
    var args = Array.prototype.slice.call(arguments);
-   
-   // I would not store an infinite amount of entries,
-   // so remove oldest one if 10 stored
    if (consoleBuffer.length === 10) consoleBuffer.pop();
-
-   // remember
    consoleBuffer.push(args);
-
-   // call original function
    origLog.apply(console, args);
 };`;
 
@@ -22,17 +32,21 @@ let dp = `
 let fv=consoleBuffer.flat();
 console.log(fv)
 for(let i = 0; i < fv.length ;i++) {
-  document.getElementById('dp').innerHTML += '<li>'+fv[i]+'</li>';
-}
-`;
+  if (typeof fv[i] === 'object') {
+    document.getElementById('dp').innerHTML += '<li><pre>'+ JSON.stringify(fv[i], null,2) +'</pre></li>';
+  }
+  else if (typeof fv[i] === 'number') {
+    document.getElementById('dp').innerHTML += '<li><pre style="color: #d0782a">'+ fv[i] +'</pre></li>';
+  }
+  else document.getElementById('dp').innerHTML += '<li><pre>'+ fv[i] +'</pre></li>';
+}`;
 
-function htmlToBlob (html, css, js) {
+export default function cosLogs (html, js) {
   const getBlobURL = (code, type) => {
     const blob = new Blob([code], { type })
     return URL.createObjectURL(blob)
   }
 
-  let cssURL = getBlobURL(css, 'text/css');
   let jsURL = getBlobURL(ov + js, 'text/javascript');
   let dpURL = getBlobURL(dp, 'text/javascript');
 
@@ -40,41 +54,27 @@ function htmlToBlob (html, css, js) {
 <!doctype html>
 <html>
   <head>
+  <title>console</title>
   <meta http-equiv="Content-type" content="text/html;charset=UTF-8">
-  <link rel="stylesheet" type="text/css" href="${cssURL}" />  
   <style>
-  *,
-*::before,
-*::after {
-  box-sizing: border-box;
-}
-
-html {
-  font-family: sans-serif;
-  line-height: 1.15;
-  -webkit-text-size-adjust: 100%;
-  -webkit-tap-highlight-color: rgba(0, 0, 0, 0);
-}
-    body { margin:0; padding0;   overflow-x: hidden; }
-    #dp {
-      margin:0;
-      list-style:none;
-      width:100%; height:50%; 
-      position: fixed; 
-      bottom:0; 
-      left:0; 
-      background-color: #333333;
-      color: #dadada; padding:0;    
-      border-top: 10px solid #222222;  
-      overflow-y:auto;
-    }
-    #dp  li {border-bottom: 1px solid #393838; padding:15px;     word-break: break-all; }
+  *,*::before,*::after {box-sizing: border-box;}
+    body { background-color: #333333; margin:0; padding:0; overflow-x: hidden; }
+    #dp { margin:0; list-style:none; width:100%; padding:0; }
+    #dp  li {border-bottom: 1px solid #393838; padding:10px 15px;  word-break: break-all; }
+    p { margin:0; background-color: #222222; color: #fff; padding:10px 15px; font-size: 12px; }  
+    pre {color: #c3cb59;  margin:0; font-size: 14px; } 
+    
+::-webkit-scrollbar {width: 7px;}
+::-webkit-scrollbar-track {background: #474141;}
+::-webkit-scrollbar-thumb {background: #535151;}
+::-webkit-scrollbar-thumb:hover {background: #7c7c7c;}
   </style>
   </head>
 
   <body>
-    ${html}   
+    <div style="display: none">${html}</div>
 
+    <p>Console</p>
     <ul id="dp"></ul>
     <script src="${jsURL}"></script>
     <script src="${dpURL}"></script>
